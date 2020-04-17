efc1463496b63fe18f57fe182c888948d17d6d9e
AgustinFranceschini <62911280+agus2306@users.noreply.github.com>
2020-04-17 12:51:54 -0300

    Agregada Animacion A Paneles
    He animado los paneles de modo que se deslicen y aparezcan en la pantalla

---
+++ Resuelto/Assets/Scripts/UI/Paneles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paneles : MonoBehaviour
+{
+    public GameObject Panel_Izq;
+    public GameObject Panel_Der;
+    
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Iniciar_Animacion();
+    }
+
+    public void Iniciar_Animacion()
+    {
+        if (GetComponent<GameManager>().isGameOn == true)
+        {
+            Abrir_Paneles();
+        }
+    }
+
+    public void Abrir_Paneles()
+    {
+        if (Panel_Izq != null)
+        {
+            Animator animator = Panel_Izq.GetComponent<Animator>();
+
+            if (animator != null)
+            {
+                bool GameOn = animator.GetBool("Juego_Iniciado");
+            }                 
+        }
+        if (Panel_Der != null)
+        {
+            Animator animator = Panel_Der.GetComponent<Animator>();
+
+            if (animator != null)
+            {
+                bool GameOn = animator.GetBool("Juego_Iniciado");
+            }
+        }
+    }
+}
